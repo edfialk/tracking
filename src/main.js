0083f2b48fe7ef5ac693b021fbeda390660ae0a6
Ed Fialkowski <edfialk@gmail.com>
2019-05-31 19:45:42 -0700

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 new Vue({
