0482f192739d0dc5985a07c3097ed3a67e45a6b2
Ed Fialkowski <edfialk@gmail.com>
2019-09-17 11:49:48 -0700

    gitignore

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyA43JIA-526wPYG52udFTmqfRtgwZCE91A",
-    authDomain: "lerb-7790f.firebaseapp.com",
-    databaseURL: "https://lerb-7790f.firebaseio.com",
-    projectId: "lerb-7790f",
-    storageBucket: "lerb-7790f.appspot.com",
-    messagingSenderId: "384734768546",
-    appId: "1:384734768546:web:04de6a1e9b8d7f67"
-};
